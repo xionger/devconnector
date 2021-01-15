b280f8e5b76289f019464fcae79b9a586b4306ea
Xunhao Xiong <xxiong42@gatech.edu>
2021-01-14 23:33:38 -0600

    validate request body

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult } = require('express-validator/check');
 
 //@route        POST api/users
 //@desc         Register user
 //@access       Public
-router.post('/', (req, res) => {
-    console.log("request body: ", req.body);
-    res.send("user post api");
+router.post('/', 
+            [
+                check('name', 'Name is required').not().isEmpty(),
+                check('email', 'Please include a valid email').isEmail(),
+                check('password', 'At least 6 characters').isLength({min: 6})
+            ], 
+            (req, res) => {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({errors: errors.array()});
+                }
+                res.send("user post api");
 });
 
 module.exports = router;
